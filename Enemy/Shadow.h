a337f56b1774d264380da1863c2120236eb0eeef
takumi1115 <takumi1115347@yahoo.co.jp>
2021-12-10 15:37:55 +0900

    enemy.cppと.hを作成しました

---
+++ Enemy/Shadow.h
@@ -0,0 +1,20 @@
+//=============================================================================
+//
+// 丸影 [shadow.h]
+// Author : TAKUMI SUZUKI
+//
+//=============================================================================
+#pragma once
+#include "main.h"
+
+//*****************************************************************************
+// プロトタイプ宣言
+//*****************************************************************************
+HRESULT InitShadow(void);
+void UninitShadow(void);
+void UpdateShadow(void);
+void DrawShadow(void);
+int CreateShadow(XMFLOAT3 pos, float radius);
+void MoveShadow(int nShadow, XMFLOAT3 pos);
+void ReleaseShadow(int nShadow);
+
