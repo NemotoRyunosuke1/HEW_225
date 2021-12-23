d11510d49389703c9edbaf40e3e13335238e6826
NemotoRyunosuke <nemoto992913@outlook.jp>
2021-12-23 15:53:31 +0900

    なんかPを押すと世界が変わる

--- DX11Game/EffectManager.cpp
+++ DX11Game/EffectManager.cpp
@@ -7,6 +7,7 @@
 
 #include "EffectManager.h"
 #include "input.h"
+#include "Camera.h"
 
 Effekseer::EffectRef m_effcet;
 Effekseer::EffectRef m_handle;
@@ -68,20 +69,40 @@ int EffectManager::Load(const char *Create)
 
 int EffectManager::Play(int Handle)
 {
+	//// エフェクトのカメラ設定
+
+	CCamera* pCamera = CCamera::Get();
+
+	// 行列
+	DirectX::XMFLOAT3 cameraPos = pCamera->GetPos();
+	DirectX::XMFLOAT3 cameraLook = pCamera->GetLook();
+	DirectX::XMFLOAT3 cameraUp = pCamera->GetUp();
+	Effekseer::Vector3D eye = ::Effekseer::Vector3D(cameraPos.x, cameraPos.y, cameraPos.z);
+	Effekseer::Vector3D look = ::Effekseer::Vector3D(cameraLook.x, cameraLook.y, cameraLook.z);
+	Effekseer::Vector3D up = ::Effekseer::Vector3D(cameraUp.x, cameraUp.y, cameraUp.z);
+
+	// 投影行列を設定
+	m_renderer->SetProjectionMatrix(::Effekseer::Matrix44().PerspectiveFovLH(
+		pCamera->GetFOV(), pCamera->GetAspect(), pCamera->GetNearClip(), pCamera->GetFarClip()));
+
+	// カメラ行列を設定
+	m_renderer->SetCameraMatrix(::Effekseer::Matrix44().LookAtLH(eye, look, up));
+
 	// Handleに基づいてエフェクトの再生
 	// Effekseer
 	static int time = 0;
 
 
-	if (GetKeyPress('P'))
+	if (GetKeyTrigger('P'))
 	{
-		m_handle = m_manager->Play(m_effect, 500, 500, 0);
+		m_handle = m_manager->Play(m_effect, 0, 500, 0);
 	}
 	m_manager->Update();
 	m_renderer->SetTime(time++ / 60.0f);
 	m_renderer->BeginRendering();
 	m_manager->Draw();
-	m_renderer->EndRendering();
+	
+	//m_renderer->EndRendering();
 
 	return 0;
 }
