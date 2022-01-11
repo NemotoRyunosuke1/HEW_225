150570aefe733ed3b7763e134a3042390c1df665
NutsMikan <naonaomegane@gmail.com>
2022-01-11 18:44:50 +0900

    風を少し変更しました

--- DX11Game/EffectManager.cpp
+++ DX11Game/EffectManager.cpp
@@ -305,8 +305,8 @@ int EffectManager::Play(int Handle)
 			m_manager->SetScale(m_handle, 10.0f, 10.0f, 10.0f);   //大きさ
 
 			// 真ん中向かい風
-			m_handle = m_manager->Play(m_effect2, -1000.0f, 700.0f, 200.0f);      //表示＆座標
-			m_manager->SetScale(m_handle, 10.0f, 10.0f, 10.0f);   //大きさ
+			m_handle = m_manager->Play(m_effect2, -1000.0f, 700.0f, 0.0f);      //表示＆座標
+			m_manager->SetScale(m_handle, 12.0f, 10.0f, 12.0f);   //大きさ
 			m_manager->SetRotation(m_handle, 0.0f, 59.7f, 0.0f);
 
 			// ゴール前手前左追い風
@@ -331,8 +331,35 @@ int EffectManager::Play(int Handle)
 
 		}
 		break;
-	case STAGE_5:
 
+	case STAGE_5:
+		if (time > 130.0f)
+		{
+			// 真ん中向かい風
+			time = 0;
+			m_handle = m_manager->Play(m_effect2, -1000.0f, 500.0f, -500.0f);      //表示＆座標
+			m_manager->SetScale(m_handle, 12.0f, 10.0f, 12.0f);   //大きさ
+			m_manager->SetRotation(m_handle, 0.0f, 59.7f, 0.0f);
+
+			// 左追い風1
+			m_handle = m_manager->Play(m_effect2, -500.0f, 500.0f, 1000.0f);      //表示＆座標
+			m_manager->SetScale(m_handle, 10.0f, 10.0f, 10.0f);   //大きさ
+			m_manager->SetRotation(m_handle, 0.0f, 80.0f, 0.0f);
+
+			// 左追い風2
+			m_handle = m_manager->Play(m_effect2, -500.0f, 500.0f, 2800.0f);      //表示＆座標
+			m_manager->SetScale(m_handle, 10.0f, 10.0f, 10.0f);   //大きさ
+			m_manager->SetRotation(m_handle, 0.0f, 80.0f, 0.0f);
+
+			// 上昇気流1
+			m_handle = m_manager->Play(m_effect, -2500.0f, 300.0f, 1000.0f);      //表示＆座標
+			m_manager->SetScale(m_handle, 5.0f, 6.0f, 5.0f);   //大きさ
+
+			// 上昇気流2
+			m_handle = m_manager->Play(m_effect, -900.0f, 500.0f, 1800.0f);      //表示＆座標
+			m_manager->SetScale(m_handle, 5.0f, 6.0f, 5.0f);   //大きさ
+
+		}
 		break;
 
 	default:
--- DX11Game/windManager.cpp
+++ DX11Game/windManager.cpp
@@ -46,9 +46,10 @@ WindManager::WindManager(EStage stage)
 	{
 	case STAGE_1:
 		m_pWind[0].Create(XMFLOAT3(-1700.0f, 200.0f, 0.0f), XMFLOAT3(200.0f, 500.0f, 300.0f), XMFLOAT3(-1.0f, 1.0f, 0.0f));    // 1個目上昇気流
-		m_pWind[1].Create(XMFLOAT3(-1700.0f, 500.0f, 800.0f), XMFLOAT3(200.0f, 500.0f, 300.0f), XMFLOAT3(1.0f, 1.0f, 0.0f));   // 2個目上昇気流
-		m_pWind[2].Create(XMFLOAT3(-100.0f, 800.0f, 1300.0f), XMFLOAT3(300.0f, 300.0f, 1200.0f), XMFLOAT3(0.0f, 0.0f, 1.0f));  // 追い風
+		m_pWind[1].Create(XMFLOAT3(-1700.0f, 300.0f, 800.0f), XMFLOAT3(200.0f, 500.0f, 300.0f), XMFLOAT3(1.0f, 1.0f, 0.0f));   // 2個目上昇気流
+		m_pWind[2].Create(XMFLOAT3(-200.0f, 800.0f, 1300.0f), XMFLOAT3(300.0f, 300.0f, 1200.0f), XMFLOAT3(0.0f, 0.0f, 1.0f));  // 追い風
 		break;
+
 	case STAGE_2:
 		m_pWind[0].Create(XMFLOAT3(-80.0f, 500.0f, -600.0f), XMFLOAT3(300.0f, 300.0f, 400.0f), XMFLOAT3(0.0f, 0.0f, -1.0f));  // 向かい風1
 		m_pWind[1].Create(XMFLOAT3(-700.0f, 500.0f, -600.0f), XMFLOAT3(300.0f, 300.0f, 400.0f), XMFLOAT3(0.0f, 0.0f, -1.0f));  // 向かい風2
@@ -83,6 +84,12 @@ WindManager::WindManager(EStage stage)
 
 		break;
 	case STAGE_5:
+		m_pWind[0].Create(XMFLOAT3(-1000.0f, 500.0f, -500.0f), XMFLOAT3(500.0f, 600.0f, 400.0f), XMFLOAT3(0.0f, 0.0f, -1.0f));   // 真ん中向かい風
+		m_pWind[1].Create(XMFLOAT3(-500.0f, 500.0f, 1000.0f), XMFLOAT3(100.0f, 300.0f, 300.0f), XMFLOAT3(-1.0f, 0.0f, 1.0f));    // 左追い風1
+		m_pWind[2].Create(XMFLOAT3(-500.0f, 500.0f, 2800.0f), XMFLOAT3(100.0f, 600.0f, 300.0f), XMFLOAT3(-1.0f, 0.0f, 1.0f));    // 左追い風2
+		m_pWind[3].Create(XMFLOAT3(-2500.0f, 300.0f, 1000.0f), XMFLOAT3(200.0f, 400.0f, 200.0f), XMFLOAT3(1.0f, 1.0f, 0.0f));    // 上昇気流1
+		m_pWind[4].Create(XMFLOAT3(-900.0f, 500.0f, 1800.0f), XMFLOAT3(200.0f, 300.0f, 200.0f), XMFLOAT3(1.0f, 1.0f, 0.0f));     // 上昇気流2
+
 		break;
 	case MAX_STAGE:
 		break;
