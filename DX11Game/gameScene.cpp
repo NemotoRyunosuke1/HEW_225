92d45ea857f8190a3e077dc6d3462c1798bca92c
fujisaki1090 <fujisaki1090@gmail.com>
2022-01-18 16:19:35 +0900

    味方の配置の変更

--- DX11Game/gameScene.cpp
+++ DX11Game/gameScene.cpp
@@ -310,10 +310,10 @@ GameScene::GameScene(EStage stage)
 		//CrewCreate(XMFLOAT3(-3200.0f, 675.0f, 300.0f));// 5
 		//CrewCreate(XMFLOAT3(-3000.0f, 500.0f, 700.0f));// 6
 
-		CrewCreate(XMFLOAT3(-1000.0f, 850.0f, 2500.0f));// 7
-		CrewCreate(XMFLOAT3(-1200.0f, 850.0f, 2800.0f));// 8
-		CrewCreate(XMFLOAT3(-800.0f, 850.0f, 3200.0f));// 9
-		CrewCreate(XMFLOAT3(-1000.0f, 850.0f, 3500.0f));// 10
+		CrewCreate(XMFLOAT3( -100.0f, 850.0f, 2400.0f));// 7
+		CrewCreate(XMFLOAT3( -500.0f, 850.0f, 2700.0f));// 8
+		CrewCreate(XMFLOAT3( -600.0f, 850.0f, 3200.0f));// 9
+		CrewCreate(XMFLOAT3( -900.0f, 850.0f, 3500.0f));// 10
 
 
 		// ゴール位置初期化
@@ -344,13 +344,13 @@ GameScene::GameScene(EStage stage)
 
 		// 仲間の配置
 		CrewCreate(XMFLOAT3( -700.0f, 500.0f, -1300.0f));// 1
-		CrewCreate(XMFLOAT3(-1900.0f, 500.0f, -600.0f));// 2
-		CrewCreate(XMFLOAT3(-1000.0f, 500.0f, 0.0f));// 3
-		CrewCreate(XMFLOAT3(-100.0f, 500.0f, 600.0f));// 4
-		CrewCreate(XMFLOAT3(-1000.0f, 500.0f, 1500.0f));// 5
-		CrewCreate(XMFLOAT3(-1000.0f, 200.0f, 1800.0f));// 6
-		CrewCreate(XMFLOAT3(-1000.0f, 800.0f, 2200.0f));// 7
-		CrewCreate(XMFLOAT3(-1000.0f, 500.0f, 2500.0f));// 8
+		CrewCreate(XMFLOAT3(-1900.0f, 500.0f,  -600.0f));// 2
+		CrewCreate(XMFLOAT3(-1000.0f, 500.0f,  -200.0f));// 3
+		CrewCreate(XMFLOAT3( -100.0f, 500.0f,   600.0f));// 4
+		CrewCreate(XMFLOAT3(-1000.0f, 500.0f,  1500.0f));// 5
+		CrewCreate(XMFLOAT3(-1000.0f, 200.0f,  1800.0f));// 6
+		CrewCreate(XMFLOAT3(-1000.0f, 800.0f,  2200.0f));// 7
+		CrewCreate(XMFLOAT3(-1000.0f, 500.0f,  2500.0f));// 8
 
 		// ゴールUI位置初期化
 		SetGoalUI(XMFLOAT3(-1000.0f, 600.0f,2000.0f), 1200, 600, XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f), 0);
@@ -478,7 +478,7 @@ GameScene::GameScene(EStage stage)
 		}
 		// 仲間の配置
 		CrewCreate(XMFLOAT3(-100.0f, 500.0f, -1000.0f));// 1
-		//CrewCreate(XMFLOAT3(1000.0f, 500.0f, 0.0f));// 2
+		//CrewCreate(XMFLOAT3(1000.0f, 500.0f, -100.0f));// 2
 		CrewCreate(XMFLOAT3(-100.0f, 500.0f, 500.0f));// 3
 		CrewCreate(XMFLOAT3(-1500.0f, 500.0f, 0.0f));// 4
 		CrewCreate(XMFLOAT3(-1900.0f, 200.0f, -500.0f));// 5
